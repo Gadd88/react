5f30d273c14a0b224ef271d67d3136c1a26f7c2a
Gadd88 <matiassaade@gmail.com>
2023-07-31 21:12:44 -0300

    added forms components and style with styled-components

--- 04-proyecto-seguros/src/App.jsx
+++ 04-proyecto-seguros/src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
+import Header from "./components/Header"
+Header
+import styled from '@emotion/styled'
+import Form from "./components/Form"
 
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContenedorForm = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Contenedor>
+      <Header
+        titulo='Cotizador de Seguros'
+        />
+      <ContenedorForm>
+        <Form />
+      </ContenedorForm>
+    </Contenedor>
   )
 }
 
