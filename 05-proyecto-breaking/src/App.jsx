3a5fbeff645f305fd8c225f4f85cc1b8d29f5637
Gadd88 <matiassaade@gmail.com>
2023-08-09 16:21:15 -0300

    created components using styled-components, testing the api response

---
+++ 05-proyecto-breaking/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import styled from 'style-components'
+
+function App() {
+
+  const [frase, setFrase] = useState({})
+
+  const consultarApi = async () =>{
+    const api = await fetch('https://api.breakingbadquotes.xyz/v1/quotes')
+    const frase = await api.json()
+    setFrase(frase)
+  }
+
+  return (
+    <Contenedor>
+      <Boton
+        onClick={consultarApi}
+      > 
+        Obtener Frase
+      </Boton>
+    </Contenedor>
+  )
+}
+
+export default App
+
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: 'Arial', Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+
+`
