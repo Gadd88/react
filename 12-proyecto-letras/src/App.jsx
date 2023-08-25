58e05317be35217f334e98bbaf93f6d58a0befc3
Gadd88 <matiassaade@gmail.com>
2023-08-24 21:24:08 -0300

    creados states en formulario para consultar a apis

--- 12-proyecto-letras/src/App.jsx
+++ 12-proyecto-letras/src/App.jsx
@@ -1,3 +1,4 @@
+
 import Formulario from "./components/Formulario"
 
 function App() {
--- 12-proyecto-letras/src/components/Formulario.jsx
+++ 12-proyecto-letras/src/components/Formulario.jsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
 
+const Formulario = ({setConsulta}) => {
 
-const Formulario = () => {
+    const [busqueda, setBusqueda] = useState({
+        artista:'',
+        cancion:'',
+    })
+    const [error, setError] = useState(false)
+
+    const {artista, cancion} = busqueda
+    
+
+    const actualizarBusqueda = (e) =>{
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    //consulta para Api
+    const buscarInfo = (e) =>{
+        e.preventDefault;
+
+        if(artista.trim==='' || cancion.trim===''){
+            setError(true)
+            return
+        }
+
+        setError(false)
+        setConsulta(busqueda)
+        setBusqueda({
+            artista:'',
+            cancion:''
+        })
+    }
+
   return (
     <div className="bg-info">
         <div className="container">
             <div className="row">
+                {
+                    error 
+                        ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p>
+                        : null
+                }
                 <form
                     className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    onSubmit={buscarInfo}
                     >
                         <fieldset>
                             <legend className="text-center">Buscador de Letras</legend>
@@ -18,7 +58,9 @@ const Formulario = () => {
                                             type="text" 
                                             className="form-control"
                                             name='artista'
-                                            placeholder='Nombre del Artista'/>
+                                            placeholder='Nombre del Artista'
+                                            onChange={actualizarBusqueda}
+                                            value={artista}/>
                                     </div>
                                 </div>
                                 <div className="col-md-6">
@@ -28,10 +70,17 @@ const Formulario = () => {
                                             type="text" 
                                             className="form-control"
                                             name='cancion'
-                                            placeholder='Nombre de la canción'/>
+                                            placeholder='Nombre de la canción'
+                                            onChange={actualizarBusqueda}
+                                            value={cancion}/>
                                     </div>
                                 </div>
                             </div>
+                            <button 
+                                type="submit" 
+                                value="Buscar"
+                                className="btn btn-primary mt-2 float-right">Buscar</button>
+                            
                         </fieldset>
                 </form>
             </div>
