03a440ba41bbcf548a05b98060b38af713186b91
Gadd88 <matiassaade@gmail.com>
2023-08-30 02:34:03 -0300

    terminado

--- 13-proyecto-bebidas-context/src/components/Modal.jsx
+++ 13-proyecto-bebidas-context/src/components/Modal.jsx
@@ -9,7 +9,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 375,
     height: 600,
     overflow: 'auto',
     bgcolor: 'background.paper',
